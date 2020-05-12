713357a6df24511f19514107aaba8cd20a14c3a1
David Dimon <david_dimon@hotmail.com>
2020-05-12 01:04:08 -0300

    fix: selection

--- ios/RNSelectableTextView.m
+++ ios/RNSelectableTextView.m
@@ -247,28 +247,9 @@ UITextPosition* beginning;
 
 - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
 {
-     if (!_backedTextInputView.isFirstResponder) {
+    if (!_backedTextInputView.isFirstResponder) {
         [_backedTextInputView setSelectedTextRange:nil notifyDelegate:true];
-    } else {
-        UIView *sub = nil;
-        for (UIView *subview in self.subviews.reverseObjectEnumerator) {
-            CGPoint subPoint = [subview convertPoint:point toView:self];
-            UIView *result = [subview hitTest:subPoint withEvent:event];
-
-            if (!result.isFirstResponder) {
-                NSString *name = NSStringFromClass([result class]);
-
-                if ([name isEqual:@"UITextRangeView"]) {
-                    sub = result;
-                }
-            }
-        }
-        
-        if (sub == nil) {
-            [_backedTextInputView setSelectedTextRange:nil notifyDelegate:true];
-        }
     }
-    
     return [super hitTest:point withEvent:event];
 }
 
